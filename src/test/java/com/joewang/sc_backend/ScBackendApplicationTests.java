b19806d5ca1d0a4e351c6ba8f86e36823cf67ca6
Joe Wang <2189665107@qq.com>
2026-01-15 16:33:49 +0800

    initialization

---
+++ src/test/java/com/joewang/sc_backend/ScBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joewang.sc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
